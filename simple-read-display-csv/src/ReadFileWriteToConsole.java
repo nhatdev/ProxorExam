4d5ec2ceedc2e455566110b9d4622959f688598a
Nhat Nguyen <nhat.nguyen@imperiaonline.org>
2016-07-25 18:55:24 +0300

    simple-read-display-csv

--- simple-read-display-csv/src/ReadFileWriteToConsole.java
+++ simple-read-display-csv/src/ReadFileWriteToConsole.java
@@ -1,4 +1,3 @@
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import com.csvreader.CsvReader;
 
@@ -18,15 +17,32 @@ public class ReadFileWriteToConsole {
     	// ... insert code here ...
     	// invoke writeSheet()
     	// ... insert code here ...
+        ReadFileWriteToConsole wtf = new ReadFileWriteToConsole();
+        wtf.readSheet();
+        wtf.writeSheet();
     }	
 	   
     public void readSheet() throws IOException {
-    	// ... insert code here ...
-    	//  Do not change the signature of this method.
+        CsvReader reader = new CsvReader(inFile);
+
+        String[] rowData = null;
+        for (int i = 0; i < MAXROW; i++) {
+            reader.readRecord();
+            rowData = reader.getValues();
+            for (int j = 0; j < MAXCOL; j++) {
+                sheet[i][j] = rowData[j];
+            }
+        }
+
+        reader.close();
 	}
 	   
 	public void writeSheet(){
-		// ... insert code here ...
-    	//  Do not change the signature of this method.
+	    for (int i = 0; i < MAXROW; i++) {
+            for (int j = 0; j < MAXCOL; j++) {
+                System.out.printf("[%s]", sheet[i][j]);
+            }
+            System.out.println();
+        }
 	}
 }
